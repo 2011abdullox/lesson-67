b76d0b593d26fb21b647aa5060cd0d2dee176efe
2011abdullox <abdullox20090831@gmail.com>
2025-11-24 05:59:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let header = document.getElementsByClassName('header')
+console.log(header);
+let element = document.getElementById('ul')
+console.log(ul);
+element.style.display = "flex";
+element.style.justifyContent = "space-between";
+let btn = document.getElementById('btn')
+console.log(btn);
+btn.style.width = "241px";
+btn.style.height = "30px";
+btn.style.borderRadius = "5px"
+let section = document.getElementById('section')
+console.log(section);
+let img = document.getElementById('img')
+console.log(img);
+img.style.width = "100%";
+img.style.height = "100%";
+let text = document.getElementById('text')
+console.log(text);
+text.style.textAlign = "center";
+text.style.color = "white";
+let csgo = document.getElementsByClassName('csgo')[0];
+
+csgo.style.justifyContent = "spacebetween"
+csgo.style.gap = "20px";
+let one = document.getElementById('one')
+console.log(one);
+one.style.display = "flex";
+one.style.color = "white";
+one.style.gap = "250px";
+console.log(two);
+two.style.display = "flex";
+two.style.color = "white";
+two.style.gap = "250px";
+let topul = document.getElementById('topul')
+console.log(topul);
+topul.style.display = "flex";
+topul.style.justifyContent = "space-between";
